b6bda550c71495a838a9c0b1ccf5793fdc3966b8
omar <omargs85@gmail.com>
2022-04-25 22:43:33 -0500

    clear route cache

--- setLocal.sh
+++ setLocal.sh
@@ -6,4 +6,4 @@ echo "PASSPORT_PERSONAL_ACCESS_CLIENT_ID=\"1\"" >> .env
 key="$(php artisan passport:install | grep "Client secret: " | awk '{print $NF}' | tail -n 1)"
 echo "PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET=\"$key\"" >> .env
 echo "TABLAS_A_RESPALDAR=\"tw_corporativos,tw_documentos\"" >> .env
-
+php artisan route:cache
