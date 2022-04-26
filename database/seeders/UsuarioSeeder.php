16e5f3b5a965ee10a503813c23b4e9bc4b43e7e7
omar <omargs85@gmail.com>
2022-04-25 20:01:27 -0500

    edited users for rol

--- database/seeders/UsuarioSeeder.php
+++ database/seeders/UsuarioSeeder.php
@@ -19,8 +19,14 @@ class UsuarioSeeder extends Seeder
             Corporativo::query()->whereNull('tw_usuarios_id')->first()->update(['tw_usuarios_id' => $usuario->id]);
         });
 
+        $user = Usuario::all()->first();
+        $user->email = 'test_rol_1@test.com';
+        $user->rol_usuario = 1;
+        $user->save();
+
         $user = Usuario::all()->last();
-        $user->email = 'test_user@test.com';
+        $user->email = 'test_rol_3@test.com';
+        $user->rol_usuario = 3;
         $user->save();
     }
 }
